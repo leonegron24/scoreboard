f3396bf72954783f24096cd3b86d30122e338111
Leonardo Negron <leonegron24@gmail.com>
2024-10-02 18:45:54 -0600

    Done, not optimized yet

--- app.js
+++ app.js
@@ -15,6 +15,9 @@ function scoreHome(){
     let hScore = document.getElementById("homeScore") 
     hScore.innerText = home
     doubleHome();
+        // declare a winner when a team reaches a certain score threshold - window.alert can pop up announcing the winner.
+        console.log(hScore.innerText)
+    winner();
 }
 
 
@@ -30,6 +33,9 @@ function scoreAway(){
     let aScore = document.getElementById("awayScore")
     aScore.innerText = away
     doubleAway();
+    console.log(aScore.innerText)
+    // declare a winner when a team reaches a certain score threshold - window.alert can pop up announcing the winner.
+    winner();
 }
 
 //  adding a button that allows users to subtract points from a team's score, while ensuring the score does not drop below 0.
@@ -55,16 +61,34 @@ function loseAway(){
     aScore.innerText = away
 }
 
+function winner(){
+    let aScore = document.getElementById("awayScore")
+    let hScore = document.getElementById("homeScore")
+    
+    let numA = parseInt(aScore.innerText)
+    let numH = parseInt(hScore.innerText)
 
-
-
-
-
-// declare a winner when a team reaches a certain score threshold - window.alert can pop up announcing the winner.
-
-
+    if (numA >49){
+        window.alert("Away Wins!")
+        resetScore();
+    } else if (numH > 49){
+        window.alert("Home Wins!")
+        resetScore();
+    }
+}
 
 // add a reset button that sets both teams' scores back to 0, allowing for a fresh start. This reset function could also be triggered automatically when a team wins
+function resetScore(){
+    let aScore = document.getElementById("awayScore")
+    let hScore = document.getElementById("homeScore")
+    home = 0
+    away = 0
+    aScore.innerText = 0
+    hScore.innerText = 0
+}
+
+
+
 
 
 // Try to utilize parameters and arguments within your functions to make them re-usable.
